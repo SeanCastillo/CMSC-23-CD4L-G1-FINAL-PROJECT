041e6d84da375a32d93988cec680cebbcf7f2e60
deejcruz <jdcruz12@up.edu.ph>
2026-04-24 00:56:47 +0800

    added skeleton login and signup screens with no firebase logic yet

--- my_app/lib/screens/auth/login_screen.dart
+++ my_app/lib/screens/auth/login_screen.dart
@@ -1,30 +1,63 @@
 import 'package:flutter/material.dart';
+import 'signup_screen.dart';
 
-class LoginScreen extends StatelessWidget {
-  const LoginScreen({super.key});
+class LoginScreen extends StatefulWidget {
+  @override
+  _LoginScreenState createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  final emailController = TextEditingController();
+  final passwordController = TextEditingController();
+
+  bool isLoading = false;
+
+  void login() async {
+    setState(() => isLoading = true);
+
+    await Future.delayed(Duration(seconds: 1));
+
+    setState(() => isLoading = false);
+
+    if (emailController.text.isNotEmpty &&
+        passwordController.text.isNotEmpty) {
+      Navigator.pushReplacementNamed(context, '/profile');
+    } else {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Please Fill All Required Fields.')),
+      );
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Login')),
-      body: Center(
+      appBar: AppBar(title: Text('Login')),
+      body: Padding(
+        padding: EdgeInsets.all(16),
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            ElevatedButton(     // profile (login) button
-              onPressed: () {
-                Navigator.pushNamed(context, '/profile');
-              },
-              child: const Text('Go to Profile'),
+            TextField(
+              controller: emailController,
+              decoration: InputDecoration(labelText: 'Email'),
             ),
-
-            const SizedBox(height: 16),
-
-            TextButton(       // signup button
+            TextField(
+              controller: passwordController,
+              decoration: InputDecoration(labelText: 'Password'),
+              obscureText: true,
+            ),
+            SizedBox(height: 20),
+            isLoading
+                ? CircularProgressIndicator()
+                : ElevatedButton(
+                    onPressed: login,
+                    child: Text('Login'),
+                  ),
+            TextButton(
               onPressed: () {
                 Navigator.pushNamed(context, '/signup');
               },
-              child: const Text("Don't have an account? Sign up"),
+              child: Text('Create Account'),
             ),
           ],
         ),
